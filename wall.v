da65933e9ee40c75493e527e16dd6a3c3a690dac
Blaizot <ablaizot@bu.edu>
2023-12-07 23:40:25 -0500

    reset does not work for some reason

--- wall.v
+++ wall.v
@@ -2,10 +2,11 @@
 
 
 
-module wall (pixel_clk,x,y);
+module wall (pixel_clk,x,y,reset);
     parameter START = 4;
 
     input pixel_clk;
+    input reset;
 
     output reg [10:0] x;
     output reg [10:0] y;
@@ -13,11 +14,13 @@ module wall (pixel_clk,x,y);
     wire [10:0] x_loc;
     wire [10:0] y_loc;
 
+    reg x_set;
+    reg y_set;
    
     LFSR  lfsrx
             (.i_Clk(pixel_clk),
             .i_Enable(1'b1),
-            .i_Seed_DV(1'b0),
+            .i_Seed_DV(1'b1),
             .i_Seed_Data(START), // Replication
             .o_LFSR_Data(x_loc),
             .o_LFSR_Done()
@@ -26,18 +29,39 @@ module wall (pixel_clk,x,y);
     LFSR  lfsry
             (.i_Clk(pixel_clk),
             .i_Enable(1'b1),
-            .i_Seed_DV(1'b0),
-            .i_Seed_Data(START), // Replication
+            .i_Seed_DV(1'b1),
+            .i_Seed_Data(START*3+START^2), // Replication
             .o_LFSR_Data(y_loc),
             .o_LFSR_Done()
             );
     
     always@(posedge pixel_clk) begin
-        if (x == 0) begin
-            x = x_loc;
-        end
-        if (y == 0) begin
-            y = y_loc;
+        if(reset) begin
+	        x = 0;
+            y = 0;
+           // x_set = 0;
+            //y_set = 0;
+	    end
+        else begin
+            if (x == 0) begin
+                x = x_loc;
+                x_set = 1;
+            end
+            else if (x_set === 1)
+                x = x;
+            else if (x !== 0) begin
+                x = 0;
+            end
+
+            if (y == 0) begin
+                y = y_loc;
+                y_set = 1;
+            end
+            else if (y_set === 1)
+                y = y;
+            else if (y !== 0) begin
+                y = 0;
+            end
         end
     end
     
---
+++ wall_tb.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/05/2023 11:55:54 AM
+// Design Name: 
+// Module Name: LFSR_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module wall_tb();
+    reg pixel_clk, reset;
+    parameter WALL_NUM = 10;
+    wire done;
+    wire [10:0] wall_x [WALL_NUM-1:0];
+    wire [10:0] wall_y [WALL_NUM-1:0];
+    genvar k;
+    generate
+	for (k=0; k<WALL_NUM; k=k+1) begin
+		wall #(.START(50*k+k*k+5)) w1 (.pixel_clk(pixel_clk), .x(wall_x[k]), .y(wall_y[k]));
+	end
+    endgenerate
+
+     initial begin
+        pixel_clk = 0; reset = 0;
+        #220 reset = 1;
+        #260 reset = 0;
+     end
+
+    always begin
+        #10 pixel_clk = ~pixel_clk;
+    end   
+
+endmodule
+
